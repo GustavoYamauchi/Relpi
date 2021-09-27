9652d8e0aa4f3456166fa76727d8ca24905f8bf3
Gustavo Rigor <gusrigor@icloud.com>
2021-09-27 14:39:57 -0300

    Criando o projeto

---
+++ Mini4/Mini4/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Mini4
+//
+//  Created by Gustavo Rigor on 27/09/21.
+//
+
+import SwiftUI
+import Firebase
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mini4/Pods/Target Support Files/Pods-Mini4/Pods-Mini4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mini4 : NSObject
+@end
+@implementation PodsDummy_Pods_Mini4
+@end
