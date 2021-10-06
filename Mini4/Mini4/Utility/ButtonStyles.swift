a9a32347e33e2aed10c8e63516d4aa363a917e62
Douglas Ferreira <douglas.ferreira@mobile2you.com.br>
2021-10-06 14:30:01 -0300

    feat: button styles

---
+++ Mini4/Mini4/Utility/ButtonStyles.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonStyles.swift
+//  Mini4
+//
+//  Created by Douglas Cardoso Ferreira on 05/10/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct PrimaryButton: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(Color("textPrimaryButton"))
+            .background(Color("primaryButton"))
+            .cornerRadius(50)
+            .padding(.horizontal, 20)
+    }
+}
+
+struct SecondaryButton: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(Color("textSecondaryButton"))
+            .background(Color("secondaryButton"))
+            .cornerRadius(50)
+            .overlay(RoundedRectangle(cornerRadius: 50)
+                        .stroke(Color("borderSecondaryButton"), lineWidth: 2))
+            .padding(.horizontal, 20)
+    }
+}
