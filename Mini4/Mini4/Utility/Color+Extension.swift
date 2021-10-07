93e58a8dd1b850a9ab7f02596226ef3203008355
Douglas Cardoso <douglascf12@live.com>
2021-10-07 14:08:50 -0300

    fix: update colors

---
+++ Mini4/Mini4/Utility/Color+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  Color+Extension.swift
+//  Mini4
+//
+//  Created by Douglas Cardoso Ferreira on 07/10/21.
+//
+
+import SwiftUI
+
+extension Color {
+    // Cores usadas nos APP
+    static let primary = Color("primaryColor")
+    static let secondary = Color("secondaryColor")
+    static let tertiary = Color("tertiaryColor")
+    static let quaternary = Color("quaternaryColor")
+    static let quintenary = Color("quintenaryColor")
+    static let sexternary = Color("sexternaryColor")
+    
+    // Cores por componentes
+    static let primaryButton = Color("primaryButton")
+    static let textPrimaryButton = Color("textPrimaryButton")
+    static let secondaryButton = Color("secondaryButton")
+    static let textSecondaryButton = Color("textSecondaryButton")
+    static let borderSecondaryButton = Color("borderSecondaryButton")
+}
