385ccb12ff66e5393a3391fb8bb084cbecc02111
GustavoYamauchi <yamauchi_gu@hotmail.com>
2021-10-14 16:19:59 -0300

    Criando TabBar

--- Mini4/Mini4/Utility/SearchBarView.swift
+++ Mini4/Mini4/Utility/SearchBarView.swift
@@ -30,8 +30,8 @@ struct SearchBarView: View {
             }
             .background(RoundedRectangle(cornerRadius: 5).fill(Color.backgroundPrimarySearch))
             .cornerRadius(45)
-        }.padding(.horizontal, 25)
-        .padding(.bottom, 10)
+        }.padding(.horizontal, 30)
+//        .padding(.bottom, 10)
         
     }
 }
--- Mini4/Mini4/View/DoadorHome.swift
+++ Mini4/Mini4/View/DoadorHome.swift
@@ -11,40 +11,64 @@ struct DoadorHome: View {
     
     @ObservedObject var viewModel = OngViewModel()
     @State var search: String = ""
-        
+    @State var selectedItemTab = 0
+    
+    let tabItemNames = ["Home", "Favoritos", "CaixaDoacao"]
     var body: some View {
-        VStack(alignment: .leading, spacing: 30) {
-            
-            Image("logo_light")
-            
-            SearchBarView(pesquisando: $search, placeholder: "Search")
-            
-            Text("Explorar ONGs")
-                .padding(.top)
-                .foregroundColor(Color.sexternary)
-                .font(.system(size: 24, weight: .bold, design: .default))
-                .padding(.leading)
-            
-            ForEach(viewModel.data) { ong in
-                HStack {
-                    NavigationLink(destination: SobreOngView(ong: ong)) {
-                        Text("\(ong.nome)")
+        VStack{
+            ZStack{
+                
+                switch selectedItemTab {
+                case 0:
+                    VStack(alignment: .leading, spacing: 30){
+                        Image("logo_light")
+                        
+                        SearchBarView(pesquisando: $search, placeholder: "Search")
+                        
+                        Text("Explorar ONGs")
+                            .padding(.top)
+                            .foregroundColor(Color.sexternary)
+                            .font(.system(size: 24, weight: .bold, design: .default))
+                            .padding(.leading)
+                        
+                        ForEach(viewModel.data) { ong in
+                            HStack {
+                                NavigationLink(destination: SobreOngView(ong: ong)) {
+                                    Text("\(ong.nome)")
+                                }
+                                .buttonStyle(SecondaryButton())
+                            }
+                        }
+                        
+                        Button("Ver todas") {
+                            print("ver todas")
+                        }.buttonStyle(.primaryButton)
                     }
-                    .buttonStyle(SecondaryButton())
+                default:
+                    VStack{
+                        DialogCard(text: "Para favoritar ONGs e salvar itens na sua caixa de doação, faça login :)", colorStyle: .green)
+                        
+                        Button("Registrar") {
+                            print("registrar")
+                        }.buttonStyle(.textButton)
+                    }
                 }
+                
             }
             
-            Button("Ver todas") {
-                print("ver todas")
-            }.buttonStyle(.primaryButton)
+            Spacer()
             
-            DialogCard(text: "Para favoritar ONGs e salvar itens na sua caixa de doação, faça login :)", colorStyle: .green)
-            
-            Button("Registrar") {
-                print("registrar")
-            }.buttonStyle(.textButton)
-            
-            //Falta chamar Tabbar
+            HStack{
+                ForEach(0..<3){ num in
+                    Button(action: {selectedItemTab = num}, label: {
+                        Spacer()
+                        Image(tabItemNames[num])
+                            .padding(.vertical, 15)
+                            .foregroundColor(selectedItemTab == num ? .textPrimaryButton : Color.textPrimaryButton.opacity(0.5))
+                        Spacer()
+                    })
+                }
+            }.background(Color.primaryButton).cornerRadius(50).padding(.horizontal,30)
         }
     }
     
@@ -55,3 +79,4 @@ struct DoadorHome_Previews: PreviewProvider {
         DoadorHome()
     }
 }
+
