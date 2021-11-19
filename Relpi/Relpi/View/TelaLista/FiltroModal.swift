d009f6c4fea4ed10112a0b2fdf0995f9c58f4f32
Douglas Cardoso <douglascf12@live.com>
2021-11-19 16:09:15 -0300

    fix: adicionar botão de cancelar nas telas de filtros

--- Relpi/Relpi/View/TelaLista/FiltroModal.swift
+++ Relpi/Relpi/View/TelaLista/FiltroModal.swift
@@ -12,48 +12,65 @@ struct FiltroModal: View {
     @Binding var mostrarCategorias: Bool
     @Binding var mostrarApenasUrgentes: Bool
     @Binding var mostrandoView: Bool
+    @Environment(\.presentationMode) var presentationMode
+    private var estadoTela: [Bool] {
+        get { return [mostrarCategorias, mostrarApenasUrgentes, mostrarCategorias] }
+    }
     
     var body: some View {
-        VStack(alignment: .center) {
-            
-            Text("Filtrar itens")
-                .padding(.top)
-                .foregroundColor(Color.destaque)
-                .font(.system(size: 24, weight: .bold, design: .default))
-            
-            HStack(spacing: 15){
-                Text("Agrupar por categoria")
-                    .font(.system(size: 18, weight: .regular, design: .default))
+        NavigationView {
+            VStack(alignment: .center) {
                 
-                Toggle("Agrupar Categorias", isOn: $mostrarCategorias)
-                    .labelsHidden()
-                    .toggleStyle(SwitchToggleStyle(tint: .destaque))
-            }.padding(.top, 30)
-            
-            HStack(spacing: 15){
-                Text("Exibir apenas itens urgentes")
-                    .font(.system(size: 18, weight: .regular, design: .default))
+                Text("Filtrar itens")
+                    .padding(.top)
+                    .foregroundColor(Color.destaque)
+                    .font(.system(size: 24, weight: .bold, design: .default))
                 
-                Toggle("Agrupar Categorias", isOn: $mostrarApenasUrgentes)
-                    .labelsHidden()
-                    .toggleStyle(SwitchToggleStyle(tint: .destaque))
-            }.padding(.top, 30)
-            
-            Button("Aplicar filtros") {
-                mostrandoView = false
-            }.buttonStyle(.primaryButton)
-            .padding(.top, 30)
-            
-            Button("Limpar filtros") {
-                mostrarCategorias = false
-                mostrarApenasUrgentes = false
-                mostrandoView = false
-            }.buttonStyle(.secondaryButton)
-            .padding(.top, 30)
-            
+                HStack(spacing: 15){
+                    Text("Agrupar por categoria")
+                        .font(.system(size: 18, weight: .regular, design: .default))
+                    
+                    Toggle("Agrupar Categorias", isOn: $mostrarCategorias)
+                        .labelsHidden()
+                        .toggleStyle(SwitchToggleStyle(tint: .destaque))
+                }.padding(.top, 30)
+                
+                HStack(spacing: 15){
+                    Text("Exibir apenas itens urgentes")
+                        .font(.system(size: 18, weight: .regular, design: .default))
+                    
+                    Toggle("Agrupar Categorias", isOn: $mostrarApenasUrgentes)
+                        .labelsHidden()
+                        .toggleStyle(SwitchToggleStyle(tint: .destaque))
+                }.padding(.top, 30)
+                
+                Button("Aplicar filtros") {
+                    mostrandoView = false
+                }.buttonStyle(.primaryButton)
+                .padding(.top, 30)
+                
+                Button("Limpar filtros") {
+                    mostrarCategorias = false
+                    mostrarApenasUrgentes = false
+                    mostrandoView = false
+                }.buttonStyle(.secondaryButton)
+                .padding(.top, 30)
+                
+            }
+            .navigationBarItems(
+                leading: Button(action: {
+                    mostrarCategorias = estadoTela[0]
+                    mostrarApenasUrgentes = estadoTela[1]
+                    mostrandoView = estadoTela[2]
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    HStack {
+                        Text("Cancelar")
+                            .accentColor(.destaque)
+                    }
+                }
+            )
         }
-
-        
     }
 }
 
