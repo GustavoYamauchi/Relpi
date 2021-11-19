f4229d721d84e605c1c72194bed48c06401b7dbf
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-11-19 15:15:16 -0300

    adiciona loadingView ao formulario de item

--- Relpi/Relpi/View/TelaLista/ItemLista/Item/FormItem.swift
+++ Relpi/Relpi/View/TelaLista/ItemLista/Item/FormItem.swift
@@ -16,93 +16,100 @@ struct FormItem: View {
     
 
     var body: some View {
-        GeometryReader { geometry in
-            VStack{
-                
-                //Calculos para proporçao
-//                let propEditando = (itemViewModel.modo == .novoItem) ? 1.5 : 1.0
-                let geometryProp = ((geometry.size.height > 700) ? 0.03 : 0.01)// * propEditando
-                let img = (geometry.size.height > 1000) ? 0.4 : 0.3
-                
-                Image(itemViewModel.imagemNome)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geometry.size.height * CGFloat(img), height: geometry.size.height * CGFloat(img), alignment: .center)
-                    .padding(.top, geometry.size.height * CGFloat(geometryProp))
-                
-
-                Spacer()
-
-                
-            
-                ZStack{
-                    VStack (alignment: .leading, spacing: geometry.size.height * CGFloat(geometryProp)){
-                        
-                        Text(itemViewModel.titulo)
-                            .padding(.leading, 30)
-                            .padding(.top, 30)
-                            .foregroundColor(Color.primaryButton)
-                            .font(.system(size: 24, weight: .bold, design: .default))
-                        
-                        CustomTextField(text: $itemViewModel.item.nome, placeholder: "Nome").padding(.horizontal, 30)
-                        
-                        Category(array: ["Higiene","Alimento","Limpeza","Medicamento","Utensilio"], selected: $itemViewModel.item.categoria)
-                        
-                        HStack{
-                            Quantity(qtd: $itemViewModel.item.quantidade)
-                            
-                            Spacer()
-                            
-                            Text("Urgente")
-                                .foregroundColor(.textPlaceholderTextfield)
-                                .lineLimit(1)
-                                .font(.system(size: 14, weight: .bold, design: .default))
-                                .padding(.trailing, 10)
-                            
-                            Toggle("Agrupar Categorias", isOn: $itemViewModel.item.urgente)
-                                .labelsHidden()
-                                .toggleStyle(SwitchToggleStyle(tint: .destaque))
-                            
-                        }.padding(.horizontal, 30)
-                        
-                        if itemViewModel.apresentaFeedback {
-                            DialogCard(text: itemViewModel.mensagem, colorStyle: itemViewModel.cor)
-                        }
-                        
-                        Button("Salvar", action: {
-                            itemViewModel.salvar()
-                            novaTela.toggle()
-                            presentationMode.wrappedValue.dismiss()
-                        }).buttonStyle(.primaryButton)
-                        
-                        
-                        
-                        Button(((itemViewModel.item.visivel) ? "Ocultar" : "Desocultar"), action: {
-                            itemViewModel.item.visivel.toggle()
-                        }).buttonStyle(.secondaryButton)
-                        
-                        
-                        if itemViewModel.modo == .editarItem {
-                            Button("Excluir", action: {
-                                itemViewModel.excluirItem()
-                            }).buttonStyle(.deleteButton)
-                        }
-                        else {
-                            Button("Cancelar", action: {
-                                presentationMode.wrappedValue.dismiss()
-                            }).buttonStyle(.deleteButton)
-                        }
-                        
-                        
-                    }
-                    .background(Color.primaria).cornerRadius(30, corners: [.topLeft, .topRight])
+        ZStack {
+            GeometryReader { geometry in
+                VStack{
                     
-                }.frame(width: UIScreen.main.bounds.size.width)
+                    //Calculos para proporçao
+    //                let propEditando = (itemViewModel.modo == .novoItem) ? 1.5 : 1.0
+                    let geometryProp = ((geometry.size.height > 700) ? 0.03 : 0.01)// * propEditando
+                    let img = (geometry.size.height > 1000) ? 0.4 : 0.3
+                    
+                    Image(itemViewModel.imagemNome)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geometry.size.height * CGFloat(img), height: geometry.size.height * CGFloat(img), alignment: .center)
+                        .padding(.top, geometry.size.height * CGFloat(geometryProp))
+                    
+
+                    Spacer()
+
+                    
+                
+                    ZStack{
+                        VStack (alignment: .leading, spacing: geometry.size.height * CGFloat(geometryProp)){
+                            
+                            Text(itemViewModel.titulo)
+                                .padding(.leading, 30)
+                                .padding(.top, 30)
+                                .foregroundColor(Color.primaryButton)
+                                .font(.system(size: 24, weight: .bold, design: .default))
+                            
+                            CustomTextField(text: $itemViewModel.item.nome, placeholder: "Nome").padding(.horizontal, 30)
+                            
+                            Category(array: ["Higiene","Alimento","Limpeza","Medicamento","Utensilio"], selected: $itemViewModel.item.categoria)
+                            
+                            HStack{
+                                Quantity(qtd: $itemViewModel.item.quantidade)
+                                
+                                Spacer()
+                                
+                                Text("Urgente")
+                                    .foregroundColor(.textPlaceholderTextfield)
+                                    .lineLimit(1)
+                                    .font(.system(size: 14, weight: .bold, design: .default))
+                                    .padding(.trailing, 10)
+                                
+                                Toggle("Agrupar Categorias", isOn: $itemViewModel.item.urgente)
+                                    .labelsHidden()
+                                    .toggleStyle(SwitchToggleStyle(tint: .destaque))
+                                
+                            }.padding(.horizontal, 30)
+                            
+                            if itemViewModel.apresentaFeedback {
+                                DialogCard(text: itemViewModel.mensagem, colorStyle: itemViewModel.cor)
+                            }
+                            
+                            Button("Salvar", action: {
+                                itemViewModel.salvar()
+                                novaTela.toggle()
+                                presentationMode.wrappedValue.dismiss()
+                            }).buttonStyle(.primaryButton)
+                            
+                            
+                            
+                            Button(((itemViewModel.item.visivel) ? "Ocultar" : "Desocultar"), action: {
+                                itemViewModel.item.visivel.toggle()
+                            }).buttonStyle(.secondaryButton)
+                            
+                            
+                            if itemViewModel.modo == .editarItem {
+                                Button("Excluir", action: {
+                                    itemViewModel.excluirItem()
+                                }).buttonStyle(.deleteButton)
+                            }
+                            else {
+                                Button("Cancelar", action: {
+                                    presentationMode.wrappedValue.dismiss()
+                                }).buttonStyle(.deleteButton)
+                            }
+                            
+                            
+                        }
+                        .background(Color.primaria).cornerRadius(30, corners: [.topLeft, .topRight])
+                        
+                    }.frame(width: UIScreen.main.bounds.size.width)
+                
+                }.background(itemViewModel.item.urgente ?
+                                itemViewModel.item.visivel ? Color.urgencia : Color.urgencia.opacity(0.5) :
+                                itemViewModel.item.visivel ? Color.regular: Color.regular.opacity(0.5))
+            }
             
-            }.background(itemViewModel.item.urgente ?
-                            itemViewModel.item.visivel ? Color.urgencia : Color.urgencia.opacity(0.5) :
-                            itemViewModel.item.visivel ? Color.regular: Color.regular.opacity(0.5))
+            if itemViewModel.isLoading {
+                LoadingView()
+            }
         }
+        
     }
 }
 
