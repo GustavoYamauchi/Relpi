9129d4ecda45dc5ce082b01991d27413ec65bee3
Gustavo Rigor <gusrigor@icloud.com>
2021-11-24 15:25:28 -0300

    quando a imagem for modificada o botao tbm aparecerá

--- Relpi/Relpi/View/ONG/Organizacao/NewOngFormView.swift
+++ Relpi/Relpi/View/ONG/Organizacao/NewOngFormView.swift
@@ -86,7 +86,7 @@ struct NewOngFormView: View {
                         }.foregroundColor(Color("primaryButton"))
                         .font(.system(size: 16, weight: .bold, design: .default))
                         
-                    if (viewModel.modo == .perfil || pageIndex == 3) && viewModel.ong != viewModel.ongInicial{
+                        if (viewModel.modo == .perfil || pageIndex == 3) && (viewModel.ong != viewModel.ongInicial || viewModel.selectedImage != nil && viewModel.selectedImage?.pngData() != viewModel.downloadedImage?.pngData()){
                         Button("Salvar") {
                             viewModel.salvar()
                         }.buttonStyle(PrimaryButton())
